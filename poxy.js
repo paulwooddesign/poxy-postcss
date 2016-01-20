3cb530a1280c5522c06a5b3b1756bba641c20fd1
“Paul <paulwooddesign@gmail.com>
2016-01-19 22:27:05 -0700

    Update

---
+++ poxy.js
@@ -0,0 +1,25 @@
+// Module dependencies
+var postcss = require('postcss');
+var assign = require('object-assign');
+
+// Poxy At Rules and Declarations
+var libs = [
+  require('./lib/poxy-x'),
+  require('./lib/poxy-y')
+];
+
+var defaultSettings = {
+  gutter: '20px',
+  flexbox: 'no-flex',
+  cycle: 'auto'
+};
+
+module.exports = postcss.plugin('poxy', function poxy(settings) {
+  settings = assign(defaultSettings, settings || {});
+
+  return function (css) {
+    libs.forEach(function(lib) {
+      lib(css, settings);
+    });
+  };
+});
